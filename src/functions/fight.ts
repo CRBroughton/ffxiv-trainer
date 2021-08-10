266ba63714d158d73288e494060d1f11014bc1a9
CRBroughton <crbroughton@posteo.uk>
2021-08-10 10:16:44 +0100

    added positions array added resetEncounter function

--- src/functions/fight.ts
+++ src/functions/fight.ts
@@ -1,5 +1,19 @@
 import { ref } from "vue";
+import anime from "animejs";
+import json from "@/encounters/raids/raids-savage.json";
 
 const fightID = ref<number>(0);
+const positions = ["tank1"];
 
-export { fightID };
+const resetEncounter = () => {
+  for (const iterator of positions) {
+    anime({
+      targets: `#${iterator}`,
+      translateX: json[fightID.value].phases[0][`${iterator}`].coords[0],
+      easing: "easeInOutSine",
+      duration: 400,
+    });
+  }
+};
+
+export { fightID, positions, resetEncounter };
