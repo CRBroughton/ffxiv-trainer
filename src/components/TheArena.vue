a367202d64d2ad755bed3987bcc9b250d47a7a8c
CRBroughton <crbroughton@posteo.uk>
2021-08-11 20:33:10 +0100

    animejs easing and duration now pull from json

--- src/components/TheArena.vue
+++ src/components/TheArena.vue
@@ -15,7 +15,7 @@
       <div
         class="bg-gray-700 row-span-1 flex flex-col items-center justify-center"
       >
-        <p class="p-2 text-white">Selected Raid: {{ raid[fightID].name }}</p>
+        <p class="p-2 text-white">Selected Raid: {{ json[fightID].name }}</p>
         <p class="p-2 text-white">{{ battleText }}</p>
         <button
           class="bg-gray-200 p-2 rounded bottom-0 text-gray-800"
@@ -37,7 +37,6 @@ import { fightID, positions, currentPhases } from "@/functions/fight";
 
 export default defineComponent({
   setup() {
-    const raid = json;
     const battleText = ref<string>("");
 
     const getImgUrl = function (img: string) {
@@ -55,8 +54,8 @@ export default defineComponent({
             translateY:
               json[fightID.value].phases[currentPhases.value][`${iterator}`]
                 .coords[1],
-            easing: "easeInOutSine",
-            duration: 400,
+            easing: json[fightID.value].phases[currentPhases.value].easing,
+            duration: json[fightID.value].phases[currentPhases.value].duration,
           });
           battleText.value =
             json[fightID.value].phases[currentPhases.value].popupText;
@@ -76,8 +75,8 @@ export default defineComponent({
       generate,
       battleText,
       fightID,
-      raid,
       positions,
+      json,
     };
   },
 });
